472474e4898ca19a8e1fd66a4c6a05f0441c785d
JeongJinUng <jinung.dev@gmail.com>
2021-12-14 22:43:00 +0900

    initial commit

---
+++ src/main/java/shop/newplace/NewplaceApplication.java
@@ -0,0 +1,13 @@
+package shop.newplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewplaceApplication.class, args);
+    }
+
+}
