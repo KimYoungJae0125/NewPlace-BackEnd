7940659764f81d3107b1195a64020b4dd3b5807a
saySthAbout <saysthabout@gmail.com>
2022-01-15 17:47:22 +0900

    클래스 이름 camelCase 맞춰서 수정
    변경 전: newplaceApplication~
    변경 후: newPlaceApplication~

--- src/main/java/shop/newplace/ServletInitializer.java
+++ src/main/java/shop/newplace/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(NewplaceApplication.class);
+        return application.sources(NewPlaceApplication.class);
     }
 
 }
--- src/test/java/shop/newplace/NewPlaceApplicationTests.java
+++ src/test/java/shop/newplace/NewPlaceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NewplaceApplicationTests {
+class NewPlaceApplicationTests {
 
     @Test
     void contextLoads() {
