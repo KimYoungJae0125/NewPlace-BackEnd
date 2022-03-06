7407c4d45f5e8218c9db0aad7c6789c4de122d33
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-03-06 14:26:55 +0900

    Delete JwtRefreshTokenRedisRepository.java

--- src/main/java/shop/newplace/Users/model/repository/JwtRefreshTokenRedisRepository.java
+++
@@ -1,9 +0,0 @@
-package shop.newplace.Users.model.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import shop.newplace.Users.model.entity.JwtRefreshToken;
-
-public interface JwtRefreshTokenRedisRepository extends CrudRepository<JwtRefreshToken, Long> {
-
-}
